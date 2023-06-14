353d108069c895561744f209b7418774d4046f36
ryotarofr <ryoryo.fr0608@icloud.com>
2023-06-14 11:59:17 +0900

    ➕ add post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser()
+
+  const { data } = api.posts.getAll.useQuery()
   return (
     <>
       <Head>
@@ -17,9 +18,13 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className=" bg-white">
-          {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
+          {/* {!user.isSignedIn ? <SignInButton /> : <SignOutButton />} */}
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn />
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
